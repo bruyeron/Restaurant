e53c0f76e4c6f063480a21a975aac7850046c481
Cedrick Bruyeron <crandriafara@gmail.com>
2025-05-05 17:14:12 +0300

    Resolution du conflit sur styles.css

---
+++ login/login.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const togglePassword = document.getElementById('togglePassword');
+    const passwordInput = document.getElementById('password');
+
+    togglePassword.addEventListener('click', function() {
+        const type = passwordInput.getAttribute('type') === 'password' ? 'text' : 'password';
+        passwordInput.setAttribute('type', type);
+
+        const icon = this.querySelector('i');
+        icon.classList.toggle('fa-eye');
+        icon.classList.toggle('fa-eye-slash');
+    });
+
+    document.getElementById('loginForm').addEventListener('submit', function(e) {
+        e.preventDefault();
+        const email = document.getElementById('email').value;
+        const password = document.getElementById('password').value;
+
+        if (email === "admin@gmail.com"  && password === "admin123") {
+            localStorage.setItem('loggedIn', 'true');
+            window.location.href = "../index.html";
+        } else {
+            alert("Email ou mot de passe incorrect!")
+        }
+    })
+});
